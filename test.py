9a44d44006819142fe974ad3f14b1f11f6b2ce19
otoTree <160241347+otoTree@users.noreply.github.com>
2025-03-20 09:43:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 # 客户端正确使用 POST 方法
 import requests
-response = requests.post("http://127.0.0.1:5000/list_tool", json={"server_name": "weather"})
+secret_key = "gduifgashjb8t3961jhgsdghf"
+response = requests.post("http://127.0.0.1:5000/list_tool", json={"server_name": "weather","secret_key":secret_key})
 print(response.text)
 
-response = requests.post("http://127.0.0.1:5000/call_tool", json={"server_name": "weather","tool_name":"get_alerts","args":{"state":"CA"}})
+response = requests.post("http://127.0.0.1:5000/call_tool", json={"server_name": "weather","tool_name":"get_alerts","args":{"state":"CA"},"secret_key":secret_key})
 print(response.text)
 
 
-response = requests.post("http://127.0.0.1:5000/list_server",)
+response = requests.post("http://127.0.0.1:5000/list_server",json={"secret_key":secret_key})
+print(response.text)
+
+response = requests.post("http://127.0.0.1:5000/list_files",json={"secret_key":secret_key})
+print(response.text)
+
+response = requests.post(f"http://127.0.0.1:5000/get_download_link/{secret_key}/武士.png")
 print(response.text)
